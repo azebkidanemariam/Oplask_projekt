a43a8bc74ae1bcf3aeace6a5685e07fe0bef4512
AbbasMansoori <70722051+AbbasMansoori@users.noreply.github.com>
2021-02-11 10:22:06 +0100

    test
    test

--- src/components/search.vue
+++ src/components/search.vue
@@ -3,7 +3,7 @@
       
       <input type="text" 
       id="searchInput" 
-      placeholder="I am looking for.." 
+      placeholder="I am looking for..." 
       v-model="searching"/>
       <button>Search</button>
       <div id="submitsearch" style="">
